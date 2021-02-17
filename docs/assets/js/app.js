fb7a5d9f284da6c31d07a85eaef2bd98ece6a214
Martijn Coevert <m.coevert@wewantweb.nl>
2021-02-17 15:04:21 +0100

    added facebook accessToken

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,30 +1,37 @@
-function init() {
-  var firebaseConfig = {
-    apiKey: "AIzaSyCAqmHntWMl1DiYjPnnJz--ptAH2RkhgtE",
-    authDomain: "watch-my-conversion.firebaseapp.com",
-    databaseURL: "https://watch-my-conversion.firebaseio.com",
-    projectId: "watch-my-conversion",
-    storageBucket: "watch-my-conversion.appspot.com",
-    messagingSenderId: "53850095842",
-    appId: "1:53850095842:web:cf46c445672394a0029ebb",
-    measurementId: "G-YZ5KXZEMSY"
-  };
-  firebase.initializeApp(firebaseConfig);
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyCAqmHntWMl1DiYjPnnJz--ptAH2RkhgtE",
+  authDomain: "watch-my-conversion.firebaseapp.com",
+  databaseURL: "https://watch-my-conversion.firebaseio.com",
+  projectId: "watch-my-conversion",
+  storageBucket: "watch-my-conversion.appspot.com",
+  messagingSenderId: "53850095842",
+  appId: "1:53850095842:web:cf46c445672394a0029ebb",
+  measurementId: "G-YZ5KXZEMSY"
+};
+firebase.initializeApp(firebaseConfig);
+
+function saveFacebookAccessToken(accessToken) {
   const urlParams = new URLSearchParams(window.location.search);
   const facebookId = urlParams.get('facebookId');
   console.log('Yes, got a facebookId', facebookId);
-  if (facebookId) {
+  if (facebookId && accessToken) {
     firebase.firestore().collection('facebook').doc(facebookId).update({
-      lastTriggeredDate: new Date()
+      accessToken,
+      updatedAt: new Date()
     }).then(() => {
-      console.log('Added lastTriggeredDate');
+      console.log('Added accessToken');
     })
   }
 };
+
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
     console.log('The facebook response', response)
+    if (response.status === "connected") {
+      saveFacebookAccessToken(response.authResponse.accessToken);
+      document.querySelector(".facebook-login").classList.add('hidden');
+      document.querySelector(".facebook-success").classList.remove('hidden');  
+    }
   });
 }
 
--- docs/facebook.md
+++ docs/facebook.md
@@ -1,7 +1,12 @@
 <div class="small-content">
   <p>In order to connect Facebook insights to your WatchMyConversion dashboard, we need access to your Facebook account.</p>
-  <fb:login-button 
-    scope="public_profile,email"
-    onlogin="checkLoginState();">
-  </fb:login-button>
+  <div class="facebook-login">
+    <fb:login-button 
+      scope="public_profile,email"
+      onlogin="checkLoginState();">
+    </fb:login-button>
+  </div>
+  <div class="facebook-success hidden">
+    You've successfully connected your Facebook account with WatchMyConversion.
+  </div>
 </div>
